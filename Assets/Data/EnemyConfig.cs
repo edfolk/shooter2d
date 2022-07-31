676fb74354f890c3a1998395cb19717fe8b7c60c
edfolk <96429557+edfolk@users.noreply.github.com>
2022-07-31 20:20:26 +0200

    enemy config

---
+++ Assets/Data/EnemyConfig.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyConfig", menuName = "EnemyConfig", order = 0)]
+public class EnemyConfig : ScriptableObject {
+    public float moveSpeed;
+}
